c4a91bd1058f7de1d66f0543699e92ae43ca5d61
AnnemarieBok <annemariebok@hotmail.com>
2026-09-22 15:23:05 +0200

    Associate the subnet to the nsg, added a network interface with a linux VM

--- terraform/02_azure_environment/main.tf
+++ terraform/02_azure_environment/main.tf
@@ -51,3 +51,54 @@ resource "azurerm_virtual_network" "vnet_dev" {
   }
 
 }
+
+data "azurerm_subnet" "web" {
+  name                 = "snet_web_dev"
+  virtual_network_name = azurerm_virtual_network.vnet_dev.name
+  resource_group_name  = azurerm_resource_group.rg_dev.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "web" {
+  subnet_id                 = data.azurerm_subnet.web.id
+  network_security_group_id = azurerm_network_security_group.nsg_dev.id
+}
+
+resource "azurerm_network_interface" "nic_dev" {
+	name = "nic_dev"
+	resource_group_name = azurerm_resource_group.rg_dev.name
+	location = var.location
+  ip_configuration {
+	name = "internal"
+	subnet_id = data.azurerm_subnet.web.id
+	private_ip_address_allocation = "Dynamic"
+  }
+  
+}
+
+resource "azurerm_linux_virtual_machine" "vm_dev" {
+	name = "vm_dev"
+	resource_group_name = azurerm_resource_group.rg_dev.name
+	location = var.location
+	size = "Standard_D4_v5"
+	admin_username = "adminuser"
+	network_interface_ids = [
+	  azurerm_network_interface.nic_dev.id,
+	]
+
+  admin_ssh_key {
+	username = "adminuser"
+	public_key = file("~/.ssh/id_ed25519.pub")
+  }
+
+  os_disk {
+	caching = "ReadWrite"
+	storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+	publisher = "Canonical"
+	offer     = "0001-com-ubuntu-server-jammy"
+	sku       = "22_04-lts"
+	version   = "latest"
+  }
+}
